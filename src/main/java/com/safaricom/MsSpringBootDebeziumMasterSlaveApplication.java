8425edd012409909b3a9db0f681e2185f79d980e
kuria  githinji <kurialancelot@gmail.com>
2024-03-15 10:51:03 +0300

    first commit

---
+++ src/main/java/com/safaricom/MsSpringBootDebeziumMasterSlaveApplication.java
@@ -0,0 +1,13 @@
+package com.safaricom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringBootDebeziumMasterSlaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSpringBootDebeziumMasterSlaveApplication.class, args);
+	}
+
+}
